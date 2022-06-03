86fd1447494d2889ce8f47c4adbfe2482150c61b
Ritik Chelani <100259409+ritikchelani@users.noreply.github.com>
2022-06-03 23:19:32 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
-
+const Shape = require('./shape.js')
 // declare class
+class Circle extends Shape {
+  constructor(color) {
+    super(color)
+  }
+  calculateArea() {
+    return 2 * Math.PI * (this.radius * this.radius)
+  }
+}
 
-
+module.exports = Circle
 // export class using module.exports
