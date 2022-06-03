6310aa3974c84c4c418229457f2e31e1725afa70
Ritik Chelani <100259409+ritikchelani@users.noreply.github.com>
2022-06-03 23:20:14 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
+class Shape {
+  constructor(color) {
+    if (color === undefined || color === null) {
+      this.color = "green"
+    } else {
+      this.color = color
+    }
+  }
+  drawShape() {
+
+  }
+
+  calculateArea() {
+
+  }
+}
+
+module.exports = Shape
 // export class using module.exports
